4eccdd5290eb5ed668f66af6f7a59a8fe1e851cf
SunTingchuan <suntingchuan@highgo.com>
2018-09-17 18:08:33 +0800

    第二次提交

---
+++ man/src/human.sql
@@ -0,0 +1,24 @@
+﻿DROP SEQUENCE public.human_id_seq;
+
+CREATE SEQUENCE public.human_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE public.human_id_seq
+  OWNER TO highgo;
+  
+DROP TABLE public.human;
+
+CREATE TABLE public.human
+(
+  id integer NOT NULL DEFAULT nextval('human_id_seq'::regclass),
+  name character varying,
+  sex character(1)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.human
+  OWNER TO highgo;
